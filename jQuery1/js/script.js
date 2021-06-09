e94d9594e06013bc2fbe28ca69d5e437d0875e90
Ednardo Paz <eds_paz@hotmail.com>
2021-06-09 10:00:34 -0300

    jQuery
    jQuery

---
+++ jQuery1/js/script.js
@@ -0,0 +1,17 @@
+// let lista1 = $("#lista1"); 
+
+// console.log(lista1.find(".item2").fadeOut());
+
+
+// console.log(lista1.children().fadeOut());
+
+// console.log($(`h1`).text(`lista de compras`))
+
+// $(`li`).attr("name",(i, oValue) => {
+//     return oValue + i
+// })
+// $("#lista1").find("[key=2]").remove()
+
+const selected = $(".item3").nextAll()
+
+console.log(selected)
